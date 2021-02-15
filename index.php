46bbe504fea99b1f68083dcd8572c3f85c647b49
najuus <78907290+najuus@users.noreply.github.com>
2021-02-15 19:51:10 +0530

    employ app

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2"></div>
+                <div class="col col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl-8">
+                    <table class="table">
+                        <tr>
+                            <td>EMPLOY CODE</td>
+                            <td><input type="text" class="float-control"></td>
+                        </tr>
+                        <tr>
+                            <td>EMPLOY NAME</td>
+                            <td><input type="text" class="float-control"></td>
+                        </tr>
+                        <tr>
+                            <td>DESIGNATION</td>
+                            <td><input type="text" class="float-control"></td>
+                        </tr>
+                        <tr>
+                            <td>SALARY</td>
+                            <td><input type="text" class="float-control"></td>
+                        </tr>
+                        <tr>
+                            <td>DOB</td>
+                            <td><input type="text" class="float-control"></td>
+                        </tr>
+                        <tr>
+                            <td>AGE</td>
+                            <td><input type="text" class="float-control"></td>
+                        </tr>
+                        <tr>
+                            <td>COMPANY NAME</td>
+                            <td><input type="text" class="float-control"></td>
+                        </tr>
+                        <tr>
+                            <td><button class="btn btn-danger">SUBMIT</button></td>
+                        </tr>
+                    </table>
+                    <div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2"></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
